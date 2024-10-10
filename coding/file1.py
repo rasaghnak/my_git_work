d103e72caad41535e2a8ce7fafccf4047d88fa7c
rasaghnak <rasaghnak22@gmail.com>
2024-10-09 23:33:09 -0400

    pattern 2

--- coding/file1.py
+++ coding/file1.py
@@ -4,3 +4,10 @@ for i in range(1,10):
     for j in range(0,i):
         print(i, end="")
     print()
+
+# pattern 2
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
