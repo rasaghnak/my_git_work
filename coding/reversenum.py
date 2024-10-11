d634a2066938f4bf98ed56a870a6eb741230e7cf
rasaghnak <rasaghnak22@gmail.com>
2024-10-10 23:56:00 -0400

    revnum

---
+++ coding/reversenum.py
@@ -0,0 +1,15 @@
+# Reverse Digits of A Number
+
+
+
+def revFun(digit):
+    rev_num=0
+    n=digit
+    while n !< 0:
+        lst_digit=n%10
+        rev_num=rev_num*10+lst_digit
+    return rev_num
+
+
+print(revFun(100))
+
