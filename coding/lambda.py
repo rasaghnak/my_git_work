8726b2beb46e7814a363ab4399e075935374244e
rasaghnak <rasaghnak22@gmail.com>
2024-10-14 22:43:33 -0400

    mod

--- coding/lambda.py
+++ coding/lambda.py
@@ -11,4 +11,6 @@ print(cubic_root(2))
 # syntax (lambda variable_name: func_equation)(actual_parameters)
 roots= (lambda a,b,c,x : a*(x**2)+(b*x)+c)(1,2,1,1)
 print(roots)
-
+# test
+my_2dList = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+print(my_2dList[2][1])
