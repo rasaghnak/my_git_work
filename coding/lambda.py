b2c3a4663dd247c83dc18279b00f5451444317a6
rasaghnak <rasaghnak22@gmail.com>
2024-10-14 22:35:28 -0400

    lambda function in python

---
+++ coding/lambda.py
@@ -0,0 +1,13 @@
+# example 1
+sqr= (lambda x:x**2)
+print(sqr(2))
+
+
+# example 2
+cubic_root= (lambda y : y**3)
+print(cubic_root(2)) 
+
+
+# syntax (lambda variable_name: func_equation)(actual_parameters)
+
+
