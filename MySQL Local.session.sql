85285f5dbf48a76dda12368a3520a7b9a31eaf1b
rasaghnak <rasaghnak22@gmail.com>
2024-10-16 12:56:42 -0400

    sql

---
+++ MySQL Local.session.sql
@@ -0,0 +1,9 @@
+-- Grocery list bananas (4); dark chocolate (2); Peanut butter(1)
+
+
+CREATE TABLE Grocery( ID INTEGER PRIMARY KEY  ,name TEXT , quantity INT);
+
+INSERT INTO Grocery VALUES (1, 'Bananas' , 4);
+INSERT INTO Grocery VALUES (2, 'dark chocolate' , 2);
+INSERT INTO Grocery VALUES (3, 'peanut butter' , 1);
+
