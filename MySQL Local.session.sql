cfc695fc68c0f4960ce11564c2b2b58047b4a366
rasaghnak <rasaghnak22@gmail.com>
2024-10-16 13:13:17 -0400

    sql

--- MySQL Local.session.sql
+++ MySQL Local.session.sql
@@ -3,8 +3,20 @@
 
 CREATE TABLE Grocery( ID INTEGER PRIMARY KEY  ,name TEXT , quantity INT);
 
+
+-- id is the unique identifier here which is why we used a primary key for ID 
 INSERT INTO Grocery VALUES (1, 'Bananas' , 4);
 INSERT INTO Grocery VALUES (2, 'dark chocolate' , 2);
 INSERT INTO Grocery VALUES (3, 'peanut butter' , 1);
 
 select * from Grocery
+
+
+
+CREATE TABLE fav_books(id INTEGER PRIMARY KEY, book_name TEXT, rating INT);
+
+INSERT INTO fav_books VALUES(1, "Harry potter" , 9);
+INSERT INTO fav_books VALUES(2, "Chandamama Kadalu" , 10);
+INSERT INTO fav_books VALUES(3, "Naruto" , 9)
+
+select * from fav_books
